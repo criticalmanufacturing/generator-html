cfc959dcec93ff19f98091e6f5ba5577566d4add
ptinocorodrigues <47790536+ptinocorodrigues@users.noreply.github.com>
2019-02-21 10:58:26 +0000

    Update wizard template (#19)
    * updated wizard .ts, .html and .default.ts

--- generators/wizard/templates/i18n/wizard.default.ts
+++ generators/wizard/templates/i18n/wizard.default.ts
@@ -1,11 +1,14 @@
 ﻿import i18nControls from "cmf.core.controls/src/i18n/main.default";
 
 export default {
-    TITLE: "Wizard title",
+    TITLE: "<%= wizard.name %>",
     ACTION: i18nControls.actions.FINISH,
     steps: {
         step1: {
-            TITLE: "Step 1 title"
+            TITLE: "Details"
         }
+    },
+    errors: {
+        NO_INSTANCE_FOUND: "No Instance Found"
     }
 };
