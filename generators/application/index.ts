b0599df3cc0b994bf6aa8d7162d0f672cef7ce63
Ricardo Ramalho <ricardojramalho@gmail.com>
2019-12-17 17:48:53 +0000

    Dev, Replace Bundle Path as string replacement (preserving all macros)

--- generators/application/index.ts
+++ generators/application/index.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import { HtmlGenerator , WebAppName} from "../html";
+import { HtmlGenerator, WebAppName } from "../html";
 import { Answers } from "yeoman-generator";
 import * as fs from "fs";
 export = class extends HtmlGenerator {
@@ -26,22 +26,22 @@ export = class extends HtmlGenerator {
   prompting() {
     return this.prompt([
       {
-        type    : "input",
-        name    : "appName",
-        message : `What is the application name? ${this.ctx.packagePrefix}.`,
-        default : `web`
+        type: "input",
+        name: "appName",
+        message: `What is the application name? ${this.ctx.packagePrefix}.`,
+        default: `web`
       },
       {
-        type    : "list",
-        name    : "basePackage",
-        message : "What is the base package you want to use?",
-        choices : [WebAppName.MES, WebAppName.Core , "other"]
+        type: "list",
+        name: "basePackage",
+        message: "What is the base package you want to use?",
+        choices: [WebAppName.MES, WebAppName.Core, "other"]
       },
       {
-        type    : "input",
-        name    : "otherPackage",
-        message : "What is the base package you want to use?",
-        when    : (answers: Answers): boolean => {
+        type: "input",
+        name: "otherPackage",
+        message: "What is the base package you want to use?",
+        when: (answers: Answers): boolean => {
           return answers.basePackage === "other";
         }
       }
@@ -49,7 +49,7 @@ export = class extends HtmlGenerator {
       console.log(answers);
       this.appName = `${this.ctx.packagePrefix}.${answers.appName}`;
       this.basePackage = answers.otherPackage ? answers.otherPackage : answers.basePackage;
- 	 });
+    });
   }
 
   writing() {
@@ -58,10 +58,10 @@ export = class extends HtmlGenerator {
     this.destinationRoot(path.join(this.ctx.__repositoryRoot, "apps", packageName));
 
     if (this.ctx.__config.registry) {
-      this.fs.copyTpl(this.templatePath("__.npmrc"), this.destinationPath(".npmrc"), {registry: this.ctx.__config.registry});
+      this.fs.copyTpl(this.templatePath("__.npmrc"), this.destinationPath(".npmrc"), { registry: this.ctx.__config.registry });
     }
     this.fs.copy(this.templatePath("__.npmignore"), this.destinationPath(".npmignore"));
-    this.fs.copyTpl(this.templatePath("gulpfile.js"), this.destinationPath("gulpfile.js"), {package: packageName});
+    this.fs.copyTpl(this.templatePath("gulpfile.js"), this.destinationPath("gulpfile.js"), { package: packageName });
     this.fs.copyTpl(this.templatePath("package.json"), this.destinationPath("package.json"), {
       package: packageName,
       basePackage: this.basePackage,
@@ -70,7 +70,7 @@ export = class extends HtmlGenerator {
     });
     this.fs.copy(this.templatePath("web.config"), this.destinationPath("web.config"));
     this.fs.copy(this.templatePath("manifest.json"), this.destinationPath("manifest.json"));
-    this.fs.copyTpl(this.templatePath("index.html"), this.destinationPath("index.html"), {isExtendingMes: this.basePackage === WebAppName.MES});
+    this.fs.copyTpl(this.templatePath("index.html"), this.destinationPath("index.html"), { isExtendingMes: this.basePackage === WebAppName.MES });
   }
 
   install() {
@@ -83,32 +83,50 @@ export = class extends HtmlGenerator {
     if (this.fs.exists(configPath)) {
       // Try to assign dynamic bundles location (optional task)
       try {
-
-        // Escape config JSON
+        // Copy config setup file
+        fs.copyFileSync(configPath, this.destinationPath("config.json"));
+        // Regular expression to find the bundle path within non standard config JSON
+        var regExpBundles = "\"bundlePath\"\s*\:\s*\".*\"";
+        var configReplaced = false;
+        // Read the config file within App Folder
         var configFile = fs.readFileSync(this.destinationPath("config.json"), 'utf8');
-        configFile = configFile.replace(/"\$\(.*\)"/g, "\"\"");
-        configFile = configFile.replace(/\$\(.*\)/g, '0');
-        fs.writeFileSync(this.destinationPath("config.json"), configFile);
-        var configFileJSON = JSON.parse(configFile);
+        // Try to match bundle configuration
+        var matchArray = configFile.match(regExpBundles);
+        if (matchArray != null && matchArray.length === 1) {
+          // Read the bundle configuration as JSON
+          var configFileJSON = JSON.parse("{" + matchArray[0] + "}");
+          // Fill some default values (bundlePath)
+          if (configFileJSON && "bundlePath" in configFileJSON && configFileJSON.bundlePath) {
+            var newBundleValue = configFileJSON.bundlePath;
 
+            // If the current defined folder exists, will be used
+            if (!fs.existsSync(this.destinationPath(newBundleValue))) {
+              if (newBundleValue[0] !== "/") {
+                newBundleValue = "/" + configFileJSON.bundlePath;
+              }
+              if (fs.existsSync(this.destinationPath("node_modules", this.basePackage, newBundleValue.toString()))) {
+                newBundleValue = "node_modules/" + this.basePackage + newBundleValue
+              } else {
+                newBundleValue = "";
+              }
+              configFile = configFile.replace(new RegExp(regExpBundles), `"bundlePath" : "${newBundleValue}"`);
+              // Persist setup config JSON
+              fs.writeFileSync(configPath, configFile);
+            }
+            // Persist app config JSON
+            fs.writeFileSync(this.destinationPath("config.json"), configFile);
 
-        // Fill some default values (bundlePath)
-        if (configFileJSON && configFileJSON.packages && "bundlePath" in configFileJSON.packages
-          && configFileJSON.packages.bundlePath) {
-          if (configFileJSON.packages.bundlePath[0] !== "/") {
-            configFileJSON.packages.bundlePath = "/" + configFileJSON.packages.bundlePath;
-          }
-          if (this.fs.exists(this.destinationPath("node_modules", this.basePackage, configFileJSON.packages.bundlePath))) {
-            configFileJSON.packages.bundlePath = "node_modules/" + this.basePackage + configFileJSON.packages.bundlePath;
-          } else {
-            configFileJSON.packages.bundlePath = "";
+            configReplaced = true;
           }
         }
-
-        // Persist JSON
-        fs.writeFileSync(this.destinationPath("config.json"), JSON.stringify(configFileJSON, null, "\t"));
-
-      } catch { }
+        // In worst scenario, the template config JSON will be copied
+        if (!configReplaced) {
+          this.fs.copy(this.templatePath("config.json"), this.destinationPath("config.json"));
+        }
+      }
+      catch (error) {
+        this.fs.copy(this.templatePath("config.json"), this.destinationPath("config.json"));
+      }
     } else {
       this.fs.copy(this.templatePath("config.json"), this.destinationPath("config.json"));
     }
